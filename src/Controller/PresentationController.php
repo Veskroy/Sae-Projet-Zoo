f69529c0926124851e21e93c60d363b512e1cb23
jaco0024 <logan.jacotin@gmail.com>
2023-11-11 16:56:39 +0100

    edit PresentationController : Correction du code

--- src/Controller/PresentationController.php
+++ src/Controller/PresentationController.php
@@ -8,16 +8,6 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PresentationController extends AbstractController
 {
-
-
-namespace App\Controller;
-
-    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use Symfony\Component\HttpFoundation\Response;
-    use Symfony\Component\Routing\Annotation\Route;
-
-class PresentationController extends AbstractController
-{
     #[Route('/presentation', name: 'app_presentation')]
     public function index(): Response
     {
