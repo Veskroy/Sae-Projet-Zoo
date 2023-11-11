df0cc482c2303167eb21b2cab1ce94cc1bb35246
jaco0024 <logan.jacotin@gmail.com>
2023-11-11 16:55:10 +0100

    add Route : app_presentation

--- src/Controller/PresentationController.php
+++ src/Controller/PresentationController.php
@@ -9,4 +9,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class PresentationController extends AbstractController
 {
 
+
+namespace App\Controller;
+
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+
+class PresentationController extends AbstractController
+{
+    #[Route('/presentation', name: 'app_presentation')]
+    public function index(): Response
+    {
+        return $this->render('presentation/index.html.twig', [
+            'controller_name' => 'PresentationController',
+        ]);
+    }
 }
