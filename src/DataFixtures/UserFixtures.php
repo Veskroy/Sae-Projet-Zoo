fefddcff8a40ea092ad75cc20f5f7780d5b4656d
jaco0024 <logan.jacotin@etudiant.univ-reims.fr>
2023-11-14 21:46:26 +0100

    edit Fixture : Modification de la methode load

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 ;
@@ -10,9 +11,21 @@ class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        UserFactory::CreateOne([
+            'firstname' => 'Logan',
+            'lastname' => 'Jacotin',
+            'email' => 'root@example.com',
+            'password' => 'test',
+            'roles' => ['ROLE_ADMIN'],
+        ]);
+        UserFactory::CreateOne([
+            'firstname' => 'Romain',
+            'lastname' => 'Leroy',
+            'email' => 'user@example.com',
+            'password' => 'test',
+            'roles' => ['ROLE_USER'],
+        ]);
+        UserFactory::createMany(10);
 
-        $manager->flush();
     }
 }
