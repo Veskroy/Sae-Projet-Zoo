660215715eb7daa457dea1a044a0a40ddb56ba70
Romain Leroy <romain.leroy@etudiant.univ-reims.fr>
2023-12-01 11:13:08 +0100

    Ajout du script JS du calendrier

---
+++ public/scripts/eventCalendars.js
@@ -0,0 +1,18 @@
+import * as FullCalendar from "@fullcalendar/core";
+
+document.addEventListener('DOMContentLoaded', function() {
+    let calendarEl = document.getElementById('calendar');
+    let calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        initialDate: '2023-11-07',
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        },
+        events: [
+        ]
+    });
+
+    calendar.render();
+});
