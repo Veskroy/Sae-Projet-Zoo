58bb632db835e7892ef75560e21bd316b9b6ebc6
Romain Leroy <romain.leroy@etudiant.univ-reims.fr>
2023-12-01 14:55:47 +0100

    Affichage primaire du calendrier

--- public/scripts/eventCalendars.js
+++ public/scripts/eventCalendars.js
@@ -1,16 +1,14 @@
-import * as FullCalendar from "@fullcalendar/core";
-
 document.addEventListener('DOMContentLoaded', function() {
     let calendarEl = document.getElementById('calendar');
     let calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'dayGridMonth',
-        initialDate: '2023-11-07',
         headerToolbar: {
             left: 'prev,next today',
             center: 'title',
             right: 'dayGridMonth,timeGridWeek,timeGridDay'
         },
         events: [
+
         ]
     });
 
