2429f17062007158db3019e641218a91e453b28e
Romain Leroy <romain.leroy@etudiant.univ-reims.fr>
2023-12-01 11:12:45 +0100

    Modification de la route des évènements

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,17 +2,16 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends AbstractController
 {
-    #[Route('/event', name: 'app_event')]
-    public function index(): Response
+    #[Route('/events', name: 'app_events')]
+    public function index(EventRepository $eventRepository): Response
     {
-        return $this->render('event/index.html.twig', [
-            'controller_name' => 'EventController',
-        ]);
+        return $this->render('event/index.html.twig', ['events' => $eventRepository]);
     }
 }
