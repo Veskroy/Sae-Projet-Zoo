2444ca512d9812c9940c55141ca598b5ffb533dd
Vice Kpat <vincentkpatinde14@gmail.com>
2023-12-01 01:42:17 +0100

    ADD migration. Pour les entity de base (sans relation ni autre)

---
+++ migrations/Version20231201004023.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231201004023 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE animal (id INT AUTO_INCREMENT NOT NULL, name_anm VARCHAR(30) NOT NULL, gender INT DEFAULT NULL, desc_anm VARCHAR(255) DEFAULT NULL, weight DOUBLE PRECISION DEFAULT NULL, size DOUBLE PRECISION DEFAULT NULL, birth_date DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE employee (id INT AUTO_INCREMENT NOT NULL, id_sup_emp INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE event (id INT AUTO_INCREMENT NOT NULL, namevent VARCHAR(30) DEFAULT NULL, date_event DATETIME DEFAULT NULL, hstartevent DATETIME DEFAULT NULL, hendevent DATETIME DEFAULT NULL, maxi_num_place INT DEFAULT NULL, des_event VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE family (id INT AUTO_INCREMENT NOT NULL, name_fam VARCHAR(60) DEFAULT NULL, desc_fam VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE pen (id INT AUTO_INCREMENT NOT NULL, capacity INT DEFAULT NULL, type_pen VARCHAR(50) DEFAULT NULL, size_pen DOUBLE PRECISION DEFAULT NULL, numplace INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE species (id INT AUTO_INCREMENT NOT NULL, name_spe VARCHAR(50) DEFAULT NULL, diet VARCHAR(255) DEFAULT NULL, origin VARCHAR(60) DEFAULT NULL, descspe VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE spot (id INT AUTO_INCREMENT NOT NULL, code_spot INT DEFAULT NULL, type_spot VARCHAR(10) NOT NULL, name_spot VARCHAR(30) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE ticket (id INT AUTO_INCREMENT NOT NULL, date DATETIME DEFAULT NULL, price INT DEFAULT NULL, period INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, first_name VARCHAR(30) NOT NULL, last_name VARCHAR(40) NOT NULL, phone VARCHAR(10) DEFAULT NULL, pc VARCHAR(5) NOT NULL, city VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE visitor (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE animal');
+        $this->addSql('DROP TABLE employee');
+        $this->addSql('DROP TABLE event');
+        $this->addSql('DROP TABLE family');
+        $this->addSql('DROP TABLE pen');
+        $this->addSql('DROP TABLE species');
+        $this->addSql('DROP TABLE spot');
+        $this->addSql('DROP TABLE ticket');
+        $this->addSql('DROP TABLE `user`');
+        $this->addSql('DROP TABLE visitor');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
