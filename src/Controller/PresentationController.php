5b75b6c26318a6f5565187a8a39155c8aa980219
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2023-12-13 16:25:55 +0100

    edit: affichage d'un message de type warning si un utilisateur connecté n'a pas ses informations complètes

--- src/Controller/PresentationController.php
+++ src/Controller/PresentationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,15 @@ class PresentationController extends AbstractController
     #[Route('/presentation', name: 'app_presentation')]
     public function index(): Response
     {
+        $user = $this->getUser();
+
+        if ($user instanceof User) {
+            $areInformationsFilled = $user->getPc() || $user->getAddress() || $user->getCity() || $user->getPhone();
+            if (!$areInformationsFilled) {
+                $this->addFlash('warning', 'Vos informations semblent incomplètes, complétez les en accédant à votre profil.');
+            }
+        }
+
         return $this->render('presentation/index.html.twig', [
             'controller_name' => 'PresentationController',
         ]);
