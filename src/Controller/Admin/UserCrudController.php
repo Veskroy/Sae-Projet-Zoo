4b6869245369c33ce70b5adc0a9a75c5d88b55cb
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2023-12-16 16:15:30 +0100

    add: affichage des champs d'un utilisateur

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -15,14 +16,19 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            yield IdField::new('id', 'ID')->hideOnForm(),
+            yield TextField::new('firstname','Firstname'),
+            yield TextField::new('lastname','Lastname'),
+            yield TextField::new('email','Email'),
+            yield TextField::new('phone', 'Phone'),
+            yield TextField::new('pc', 'Postal Code'),
+            yield TextField::new('city', 'City'),
+            yield TextField::new('address', 'Address'),
+            yield ArrayField::new('roles', 'Role(s)')
         ];
     }
-    */
+
 }
