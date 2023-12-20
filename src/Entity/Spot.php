4bcd95517d5e50638825e0664dab0c841dea3141
Logan Jacotin <logan.jacotin@gmail.com>
2023-12-20 11:28:23 +0100

    edit : correction de l'entité Spot

--- src/Entity/Spot.php
+++ src/Entity/Spot.php
@@ -13,42 +13,12 @@ class Spot
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(nullable: true)]
-    private ?int $code = null;
-
-    #[ORM\Column(length: 10)]
-    private ?string $type = null;
-
     #[ORM\Column(length: 30, nullable: true)]
     private ?string $name = null;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getCode(): ?int
-    {
-        return $this->code;
-    }
-
-    public function setCode(?int $code): static
-    {
-        $this->code = $code;
-
-        return $this;
-    }
-
-    public function getType(): ?string
-    {
-        return $this->type;
-    }
-
-    public function setType(string $type): static
-    {
-        $this->type = $type;
-
-        return $this;
     }
 
     public function getName(): ?string
