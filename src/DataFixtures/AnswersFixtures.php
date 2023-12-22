106452c6596582338d8897de3a3af2c2eb721683
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2023-12-23 00:38:50 +0100

    add: génération de données factices pour l'entité Answer

--- src/DataFixtures/AnswersFixtures.php
+++ src/DataFixtures/AnswersFixtures.php
@@ -2,16 +2,43 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\AnswerFactory;
+use App\Factory\QuestionFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class AnswersFixtures extends Fixture
+class AnswersFixtures extends Fixture implements DependentFixtureInterface
 {
+    /**
+     * @throws \Exception
+     */
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        //AnswerFactory::createMany(10);
+        $allUsers = UserFactory::repository()->findAll();
+        if (!empty($allUsers)) {
+            $allQuestions = QuestionFactory::repository()->findAll();
+            foreach ($allQuestions as $question) {
+                $random = range(0, 10);
+                AnswerFactory::createMany($random[array_rand($random)], function () use ($question) {
+                    return [
+                        'author' => UserFactory::random(),
+                        'question' => $question,
+                    ];
+                });
+            }
+            $manager->flush();
+        }
+        else {
+            throw new \Exception("Aucun utilisateur trouvé");
+        }
+    }
 
-        $manager->flush();
+    public function getDependencies(): array {
+        return [
+            QuestionsFixtures::class,
+        ];
     }
 }
