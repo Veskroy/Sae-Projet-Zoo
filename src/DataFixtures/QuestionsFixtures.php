6ab29ef6618ad1aa34c58ccada507d20ad16202b
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2023-12-23 00:38:37 +0100

    add: génération de données factices pour l'entité Question

--- src/DataFixtures/QuestionsFixtures.php
+++ src/DataFixtures/QuestionsFixtures.php
@@ -2,16 +2,35 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\QuestionFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
-class QuestionsFixtures extends Fixture
+class QuestionsFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        QuestionFactory::createMany(10, function () {
+            $faker = Factory::create();
+            $updatedAt = $faker->dateTimeBetween('-2 years');
+            return [
+                'author' => UserFactory::random(),
+                'updatedAt' =>
+                    $faker->boolean(80) ?
+                        \DateTimeImmutable::createFromMutable($updatedAt)
+                        : null
+            ];
+        });
 
         $manager->flush();
+    }
+
+    public function getDependencies(): array {
+        return [
+            UserFixtures::class,
+        ];
     }
 }
