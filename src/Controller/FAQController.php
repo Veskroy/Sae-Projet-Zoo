c668af9962338f4a42984f39412aeb76f9b7aabb
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2023-12-27 16:12:16 +0100

    edit: récupération des réponses triées par date croissante pour chaque question et du nombre de réponses

--- src/Controller/FAQController.php
+++ src/Controller/FAQController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
 use App\Repository\QuestionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,25 +22,35 @@ class FAQController extends AbstractController
         $questions = $questionRepository->countAnswersForAllQuestionsAndOrderByDateDesc();
 
         // récupération de toutes les questions posées par l'utilisateur courant
-        $questionsByUser = $questionRepository->findBy(['author' => $user], ['createdAt' => 'DESC']);
+        $ownQuestions = $questionRepository->findBy(['author' => $user], ['createdAt' => 'DESC']);
 
         return $this->render('faq/index.html.twig', [
             'user' => $user,
             'questions' => $questions,
-            'ownQuestions' => $questionsByUser,
+            'ownQuestions' => $ownQuestions,
         ]);
     }
 
     #[Route('/faq/{id}', name: 'app_faq_show')]
-    public function show(QuestionRepository $questionRepository): Response
+    public function show(QuestionRepository $questionRepository, Question $questionId): Response
     {
         $user = $this->getUser();
         if (!$user) {
             return $this->redirectToRoute('app_login');
         }
 
+        $question = $questionRepository->findOneBy(['id' => $questionId]);
+        $answers = $question->getAnswers()->toArray();
+        usort($answers, function ($a, $b) {
+            return $a->getCreatedAt() <=> $b->getCreatedAt();
+        });
+        $answersLength = count($answers);
+
         return $this->render('faq/show.html.twig', [
             'user' => $user,
+            'question' => $question,
+            'answers' => $answers,
+            'answersLength' => $answersLength,
         ]);
     }
 
