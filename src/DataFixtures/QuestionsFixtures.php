6d0c08ad707c9416c677018b27e2099f3c19e1b1
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2023-12-27 16:08:46 +0100

    edit: modifications sur les fixtures pour les questions et réponses

--- src/DataFixtures/QuestionsFixtures.php
+++ src/DataFixtures/QuestionsFixtures.php
@@ -7,31 +7,20 @@ use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
-use Faker\Factory;
 
 class QuestionsFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        $allUsers = UserFactory::all();
-        if (!empty($allUsers)) {
-            foreach ($allUsers as $user) {
-                QuestionFactory::createMany(3, function () use ($user) {
-                    $faker = Factory::create();
-                    $updatedAt = $faker->dateTimeBetween('-2 years');
-                    return [
-                        'author' => $user,
-                        'updatedAt' =>
-                            $faker->boolean(80) ?
-                                \DateTimeImmutable::createFromMutable($updatedAt)
-                                : null
-                    ];
-                });
-            }
-            $manager->flush();
-        } else {
-            throw new \Exception('No users found');
+        $allUsers = UserFactory::repository()->findAll();
+        foreach ($allUsers as $user) {
+            QuestionFactory::createMany(10, function () use ($user) {
+                return [
+                    'author' => $user,
+                ];
+            });
         }
+        $manager->flush();
     }
 
     public function getDependencies(): array {
