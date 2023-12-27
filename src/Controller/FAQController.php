ee028bd500c090628e35e371098d8610913a195e
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2023-12-27 17:18:45 +0100

    add: formulaire pour ajouter une réponse à une question fonctionnel

--- src/Controller/FAQController.php
+++ src/Controller/FAQController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Answer;
 use App\Entity\Question;
+use App\Form\AnswerType;
 use App\Repository\QuestionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -32,7 +37,7 @@ class FAQController extends AbstractController
     }
 
     #[Route('/faq/{id}', name: 'app_faq_show')]
-    public function show(QuestionRepository $questionRepository, Question $questionId): Response
+    public function show(QuestionRepository $questionRepository, Question $questionId, Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = $this->getUser();
         if (!$user) {
@@ -46,11 +51,40 @@ class FAQController extends AbstractController
         });
         $answersLength = count($answers);
 
+        // formulaire de réponse à une question
+
+        /* $formAnswer = $this->createFormBuilder(options: ['attr' => ['class' => 'reply-form']])
+            ->add('description')
+            ->add('submit', SubmitType::class, ['label' => 'Répondre', 'attr' => ['class' => 'btn button-primary']])
+            ->getForm(); */
+
+        $answer = new Answer();
+
+        $formAnswer = $this->createForm(AnswerType::class, $answer, ['attr' => ['class' => 'reply-form']])
+            ->add('submit', SubmitType::class, ['label' => 'Répondre', 'attr' => ['class' => 'btn button-primary full-width']]);
+
+        $formAnswer->handleRequest($request);
+
+        if ($formAnswer->isSubmitted() && $formAnswer->isValid()) {
+            //$answer->setAuthor($user);
+            $answer->setAuthor($this->getUser());
+            $answer->setQuestion($question);
+            $answer->setCreatedAt(new \DateTimeImmutable('now'));
+
+            $entityManager->persist($answer);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre réponse a bien été ajoutée!');
+            return $this->redirectToRoute('app_faq_show', ['id' => $question->getId()]);
+        }
+
+
         return $this->render('faq/show.html.twig', [
             'user' => $user,
             'question' => $question,
             'answers' => $answers,
             'answersLength' => $answersLength,
+            'formAnswer' => $formAnswer,
         ]);
     }
 
