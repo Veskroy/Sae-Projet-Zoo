bfa3f732c545d3c7c34de8d3d43709bd4a1f5fe7
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2023-12-28 19:53:24 +0100

    refactor: controlleurs modifiés pour plus de lisibilité

--- src/Controller/FAQController.php
+++ src/Controller/FAQController.php
@@ -17,7 +17,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FAQController extends AbstractController
 {
-    #[Route('/faq', name: 'app_faq')]
+    #[Route('/forum', name: 'app_forum')]
     public function index(QuestionRepository $questionRepository, Request $request, PaginatorInterface $paginator): Response
     {
         $user = $this->getUser();
@@ -41,101 +41,6 @@ class FAQController extends AbstractController
             'user' => $user,
             //'questions' => $questions,
             'pagination' => $pagination,
-        ]);
-    }
-
-    /**
-     * @throws Exception
-     */
-    #[Route('/faq/{id}', name: 'app_faq_show')]
-    public function show(QuestionRepository $questionRepository, Question $questionId, Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $user = $this->getUser();
-        if (!$user) {
-            return $this->redirectToRoute('app_login');
-        }
-
-        $question = $questionRepository->findOneBy(['id' => $questionId]);
-        $answers = $question->getAnswers()->toArray();
-        usort($answers, function ($a, $b) {
-            return $a->getCreatedAt() <=> $b->getCreatedAt();
-        });
-        $answersLength = count($answers);
-
-        // formulaire de réponse à une question
-
-        /* $formAnswer = $this->createFormBuilder(options: ['attr' => ['class' => 'reply-form']])
-            ->add('description')
-            ->add('submit', SubmitType::class, ['label' => 'Répondre', 'attr' => ['class' => 'btn button-primary']])
-            ->getForm(); */
-
-        $answer = new Answer();
-
-        $formAnswer = $this->createForm(AnswerType::class, $answer, ['attr' => ['class' => 'reply-form']])
-            ->add('submit', SubmitType::class, ['label' => 'Répondre', 'attr' => ['class' => 'btn button-primary full-width']]);
-
-        $formAnswer->handleRequest($request);
-
-        if ($formAnswer->isSubmitted() && $formAnswer->isValid()) {
-            //$answer->setAuthor($user);
-            $answer->setAuthor($this->getUser());
-            $answer->setQuestion($question);
-            $answer->setCreatedAt(new \DateTimeImmutable('now', new \DateTimeZone('Europe/Paris')));
-
-            $entityManager->persist($answer);
-            $entityManager->flush();
-
-            $this->addFlash('success', 'Votre réponse a bien été ajoutée!');
-            return $this->redirectToRoute('app_faq_show', ['id' => $question->getId()]);
-        }
-
-        // formulaire pour "réouvrir" son post ou le fermer
-
-        $formSetResolved = $this->createFormBuilder(options: ['attr' => ['class' => 'setResolved']])
-            ->add('submit', SubmitType::class)
-            ->getForm();
-
-        $formSetResolved->handleRequest($request);
-
-        if ($formSetResolved->isSubmitted() && $formSetResolved->isValid()) {
-            // utiliser isAdmin plutôt que in_array(...)
-            if ($user === $question->getAuthor() || in_array('ROLE_ADMIN', $user->getRoles())) {
-                $question->setIsResolved(!$question->isIsResolved());
-
-                $entityManager->persist($question);
-                $entityManager->flush();
-
-                $this->addFlash('success', $question->isIsResolved() ? 'Ce post a bien été clôturé!' : 'Ce post a bien été réouvert!');
-                return $this->redirectToRoute('app_faq_show', ['id' => $question->getId()]);
-            } else {
-                $this->addFlash('error', 'Vous n\'avez pas les droits requis.');
-            }
-        }
-
-        return $this->render('faq/show.html.twig', [
-            'user' => $user,
-            'question' => $question,
-            'answers' => $answers,
-            'answersLength' => $answersLength,
-            'formAnswer' => $formAnswer,
-            'formSetResolved' => $formSetResolved
-        ]);
-    }
-
-    #[Route('/faq/user/questions', name: 'app_faq_user_questions')]
-    public function userQuestions(QuestionRepository $questionRepository): Response
-    {
-        $user = $this->getUser();
-        if (!$user) {
-            return $this->redirectToRoute('app_login');
-        }
-
-        // récupération de toutes les questions posées par l'utilisateur courant
-        $ownQuestions = $questionRepository->findBy(['author' => $user], ['createdAt' => 'DESC']);
-
-        return $this->render('faq/data.html.twig', [
-            'user' => $user,
-            'ownQuestions' => $ownQuestions,
         ]);
     }
 
