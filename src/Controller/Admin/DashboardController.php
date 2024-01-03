0980472f639d43c8a1a8a93169afc3512edea997
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-01-03 23:30:45 +0100

    edit: entités Question et Answer affichées, et modification sur l'affichage de l'entité User

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Answer;
+use App\Entity\Question;
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -26,6 +29,27 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-list', User::class);
+
+        yield MenuItem::section('Utilisateurs');
+
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Ajouter un utilisateur', 'fas fa-plus', User::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Liste des utilisateurs', 'fas fa-eye', User::class),
+        ]);
+
+        yield MenuItem::section('Posts');
+
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            MenuItem::linkToCrud('Ajouter une question', 'fas fa-plus', Question::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Liste des questions', 'fas fa-eye', Question::class),
+        ]);
+
+        yield MenuItem::section('Réponses');
+
+        yield MenuItem::subMenu('Actions', 'fas fa-bars')->setSubItems([
+            //MenuItem::linkToCrud('Ajouter une réponse', 'fas fa-plus', Question::class)->setAction(Crud::PAGE_NEW),
+            MenuItem::linkToCrud('Liste des réponses', 'fas fa-eye', Answer::class),
+        ]);
+
     }
 }
