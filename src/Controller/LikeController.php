83fbdf3ebdc631155f611932c95e1aa209722efe
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2024-01-05 15:20:34 +0100

    add: action ajoutant ou supprimant le like de l'user sur une question

--- src/Controller/LikeController.php
+++ src/Controller/LikeController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LikeController extends AbstractController
 {
-    #[Route('/like', name: 'app_like')]
-    public function index(): Response
+    #[Route('/question/{id}/like', name: 'app_question_like', requirements: ['id' => '\d+'])]
+    public function like(Question $question, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('like/index.html.twig', [
-            'controller_name' => 'LikeController',
-        ]);
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        if ($question->isLikedByUser($user)) {
+            $question->removeLike($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'Votre j\'aime a bien été supprimé de ce post!');
+        } else {
+            $question->addLike($user);
+            $entityManager->flush();
+            $this->addFlash('success', 'Vous avez aimé ce post!');
+        }
+        return $this->redirectToRoute('app_question_show', ['id' => $question->getId()]);
     }
 }
