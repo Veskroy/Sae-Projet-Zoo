6d4c3dbe6e35ed7af5d0d1ef1511152261555945
jaco0024 <logan.jacotin@etudiant.univ-reims.fr>
2024-01-11 14:37:16 +0100

    edit : Relation OneToMany Spot -> Pen

--- src/Entity/Spot.php
+++ src/Entity/Spot.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\SpotRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SpotRepository::class)]
@@ -15,6 +17,14 @@ class Spot
 
     #[ORM\Column(length: 30, nullable: true)]
     private ?string $name = null;
+
+    #[ORM\OneToMany(mappedBy: 'spot', targetEntity: Pen::class)]
+    private Collection $pens;
+
+    public function __construct()
+    {
+        $this->pens = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +39,36 @@ class Spot
     public function setName(?string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Pen>
+     */
+    public function getPens(): Collection
+    {
+        return $this->pens;
+    }
+
+    public function addPen(Pen $pen): static
+    {
+        if (!$this->pens->contains($pen)) {
+            $this->pens->add($pen);
+            $pen->setSpot($this);
+        }
+
+        return $this;
+    }
+
+    public function removePen(Pen $pen): static
+    {
+        if ($this->pens->removeElement($pen)) {
+            // set the owning side to null (unless already changed)
+            if ($pen->getSpot() === $this) {
+                $pen->setSpot(null);
+            }
+        }
 
         return $this;
     }
